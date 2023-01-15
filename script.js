6c3128b81f0baeb032315a3debb7c8fb80c49fec
YK Chong <chongyongkhong.yk@gmail.com>
2023-01-15 23:28:07 +0800

    Updated app domain

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const apiDomain = "https://api-sbth.thebluelobby.com/";
-const appUrl = "https://yk-chong.github.io/sbth//";
+const appUrl = "https://yk-chong.github.io/sbth/";
 let coordinate = null;
 
 function redirectIfNoLoginRecord() {
